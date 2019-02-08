8096e8c2b70621006164aa4ca53409314c4848c4
Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>
2019-02-08 13:43:06 +0100

    switch to javascript mailto (ditch php) add first leaderboard entries
    Signed-off-by: Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>

--- src/view-models/leaderboard.js
+++ src/view-models/leaderboard.js
@@ -4,7 +4,20 @@ import {Router} from 'aurelia-router';
 @inject(Router)
 export class Leaderboard {
 
-	hackers = [];
+	hackers = [
+		{
+			name: "Anonymous",
+			github: "htd.rmg.bounty42",
+			bugsReported: 3,
+			totalReward: 17333
+		},
+		{
+			name: "Valera Cogut",
+			github: "valera-hacked-dex",
+			bugsReported: 1,
+			totalReward: 15000
+		}
+	];
 
 	constructor(router) {
 		this.router = router;
