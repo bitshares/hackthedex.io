bb99db45a28ab9692c787b301f8670e7542743d5
Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>
2019-02-08 13:47:52 +0100

    add reports
    Signed-off-by: Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>

--- src/view-models/reports.js
+++ src/view-models/reports.js
@@ -4,7 +4,38 @@ import {Router} from 'aurelia-router';
 @inject(Router)
 export class Reports {
 
-	reports = [];
+	reports = [
+		{
+			name: "20180728A",
+			description: "Cross-site scripting (XSS) code injection",
+			link: ""
+		},
+		{
+			name: "20180801A",
+			description: "Cross-site scripting (XSS) code injection",
+			link: ""
+		},
+		{
+			name: "20180807E",
+			description: "Denial of Service through broadcasting prepared transactions",
+			link: ""
+		},
+		{
+			name: "20180918A",
+			description: "Cross-site scripting (XSS) code injection",
+			link: ""
+		},
+		{
+			name: "20181008A",
+			description: "Subdomain takeover",
+			link: ""
+		},
+		{
+			name: "20181206A",
+			description: "Blockchain hault provocation",
+			link: ""
+		}
+	];
 
 	constructor(router) {
 		this.router = router;
