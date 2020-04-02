9f333643f3a75f585db71191c93e060c35215358
Fabian Schuh <mail@xeroc.org>
2020-04-02 17:39:23 +0200

    Discontinuation

--- build-prod.sh
+++ build-prod.sh
@@ -2,16 +2,16 @@
 au build --env prod
 
 rm -f deploy-prod.tar.gz
-rm -rf deploy/prod
-mkdir -p deploy/prod/node_modules
-mkdir -p deploy/prod/src/assets
+rm -rf docs
+mkdir -p docs/node_modules
+mkdir -p docs/src/assets
 
-cp index.html deploy/prod/
-cp -R config deploy/prod/
-cp -R node_modules/bootstrap deploy/prod/node_modules/
-cp -R node_modules/bootstrap-social deploy/prod/node_modules/
-cp -R node_modules/font-awesome deploy/prod/node_modules/
-cp -R scripts deploy/prod/
-cp -R src/assets deploy/prod/src/
+cp index.html docs/
+cp -R config docs/
+cp -R node_modules/bootstrap docs/node_modules/
+#cp -R node_modules/bootstrap-social docs/node_modules/
+cp -R node_modules/font-awesome docs/node_modules/
+cp -R scripts docs/
+cp -R src/assets docs/src/
 
-# cd deploy/prod && tar -cvzf ../../deploy-prod.tar.gz .
+# cd docs && tar -cvzf ../../deploy-prod.tar.gz .
